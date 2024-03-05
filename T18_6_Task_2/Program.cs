8313bce4f8fc71bdc5eb4245091bbfad9c4bb38d
Kapebah <sh1n0sh1@mail.ru>
2024-03-05 10:40:19 +0200

    add

---
+++ T18_6_Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T18_6_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
